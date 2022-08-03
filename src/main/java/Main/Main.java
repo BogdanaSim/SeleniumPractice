fb6156564d45d5eeb8637c21e15cf81661d61396
BogdanaSim <simionicabogdana@yahoo.com>
2022-08-03 20:53:19 +0300

    Changes to methods
    Added some sleeps before quitting the page for each case

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.lang.model.element.Element;
-import javax.swing.*;
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
@@ -18,82 +16,93 @@ import java.util.Random;
 
 public class Main {
 
-    public static void resetDriver(WebDriver driver) {
-        driver = new ChromeDriver();
+    public static WebDriver resetDriver() {
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://qa1magento.dev.evozon.com/");
+        return driver;
 
     }
 
-    public static void homepage(WebDriver driver) throws InterruptedException {
-        resetDriver(driver);
+
+    public static void homepage() throws InterruptedException {
+        WebDriver driver = resetDriver();
         System.out.println("Title of the current page is: " + driver.getTitle());
         System.out.println("URL of the current page is: " + driver.getCurrentUrl());
         driver.findElement(By.className("logo")).click();
         driver.get("http://qa1magento.dev.evozon.com/women/new-arrivals.html");
         driver.navigate().back();
         driver.navigate().forward();
+        Thread.sleep(1000);
         driver.navigate().refresh();
+        Thread.sleep(1000);
         driver.quit();
 
 
     }
 
-    public static void account(WebDriver driver) throws InterruptedException {
-        resetDriver(driver);
+    public static void account() throws InterruptedException {
+        WebDriver driver = resetDriver();
+        Thread.sleep(1000);
         driver.findElement(By.className("skip-account")).click();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         driver.quit();
     }
 
-    public static void languages(WebDriver driver) throws InterruptedException {
+    public static void languages() throws InterruptedException {
 
-        resetDriver(driver);
+        WebDriver driver = resetDriver();
         WebElement element = driver.findElement(By.id("select-language"));
         driver.findElement(By.id("select-language")).click();
 
         Select select = new Select(element);
-
+        Thread.sleep(2000);
         select.selectByIndex(1);
-
+        Thread.sleep(2000);
         driver.quit();
 
     }
 
-    public static void search(WebDriver driver) {
-        resetDriver(driver);
+    public static void search() throws InterruptedException {
+        WebDriver driver = resetDriver();
         WebElement element1 = driver.findElement(By.id("search_mini_form"));
         WebElement element = driver.findElement(By.id("search"));
         element.clear();
+        Thread.sleep(2000);
         element.sendKeys("woman");
         element1.submit();
+        Thread.sleep(3000);
         driver.quit();
     }
 
-    public static void newListProducts(WebDriver driver) {
-        resetDriver(driver);
+    public static void newListProducts() {
+        WebDriver driver = resetDriver();
         driver.get("http://qa2magento.dev.evozon.com/");
         List<WebElement> list = driver.findElements(By.cssSelector(".item.last"));
         System.out.println("Number of new products: " + list.size());
+        System.out.println("New products: ");
         for (WebElement element : list) {
             String s = element.getText().split("\n")[0];
             System.out.println(s);
         }
+        driver.quit();
     }
 
-    public static void navigateToPage(WebDriver driver, String pageName) throws InterruptedException {
+    public static void navigateToPage(String pageName) throws InterruptedException {
+        WebDriver driver = resetDriver();
         List<WebElement> list = driver.findElements(By.cssSelector("li.level0"));
         for (WebElement element : list) {
             if (element.getText().equalsIgnoreCase(pageName)) {
                 element.click();
+                Thread.sleep(2000);
                 break;
             }
         }
+        driver.quit();
     }
 
-    public static void addProductToCart(WebDriver driver) throws InterruptedException {
-
-        resetDriver(driver);
+    public static void addProductToCart() throws InterruptedException {
+        WebDriver driver = resetDriver();
         WebElement hover = driver.findElement(By.xpath("/html/body/div/div/header/div/div[3]/nav/ol/li[1]/a"));
         Actions actions = new Actions(driver);
         actions.moveToElement(hover).build().perform();
@@ -106,6 +115,9 @@ public class Main {
         driver.findElement(By.className("product-image")).click();
         driver.findElement(By.className("swatch-label")).click();
         driver.findElement(By.className("option-s")).click();
+        WebElement quantity=driver.findElement(By.id("qty"));
+        quantity.clear();
+        quantity.sendKeys("2");
         driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > div.add-to-cart > div.add-to-cart-buttons > button")).click();
         Thread.sleep(3000);
         driver.quit();
@@ -113,8 +125,8 @@ public class Main {
 
     }
 
-    public static void removeProductFromCart(WebDriver driver) throws InterruptedException {
-        resetDriver(driver);
+    public static void removeProductFromCart() throws InterruptedException {
+        WebDriver driver = resetDriver();
         Actions actions = new Actions(driver);
         WebElement hover = driver.findElement(By.xpath("/html/body/div/div/header/div/div[3]/nav/ol/li[1]/a"));
         actions.moveToElement(hover).build().perform();
@@ -145,8 +157,8 @@ public class Main {
 
     }
 
-    public static void submitReview(WebDriver driver) {
-        resetDriver(driver);
+    public static void submitReview() throws InterruptedException {
+        WebDriver driver = resetDriver();
         Actions actions = new Actions(driver);
         WebElement hover = driver.findElement(By.xpath("/html/body/div/div/header/div/div[3]/nav/ol/li[1]/a"));
         actions.moveToElement(hover).build().perform();
@@ -176,13 +188,14 @@ public class Main {
         driver.findElement(By.id("summary_field")).sendKeys("Awesome");
         driver.findElement(By.id("nickname_field")).sendKeys("Anonymous");
         driver.findElement(By.ByXPath.xpath("/html/body/div/div/div[2]/div/div[2]/div[3]/div[3]/div/form/div[2]/button")).click();
+        Thread.sleep(3000);
         driver.quit();
 
     }
 
 
-    public static void register(WebDriver driver) {
-        resetDriver(driver);
+    public static void register() throws InterruptedException {
+        WebDriver driver = resetDriver();
         driver.findElement(By.xpath("/html/body/div/div/header/div/div[2]/div/a/span[2]")).click();
         driver.findElement(By.xpath("/html/body/div/div/header/div/div[5]/div/ul/li[5]/a")).click();
         driver.findElement(By.id("firstname")).sendKeys("MyName");
@@ -191,25 +204,24 @@ public class Main {
         driver.findElement(By.id("password")).sendKeys("qawsed");
         driver.findElement(By.id("confirmation")).sendKeys("qawsed");
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/div[2]/form/div[2]/button")).click();
-
+        Thread.sleep(2000);
+        driver.quit();
 
     }
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://qa1magento.dev.evozon.com/");
-        homepage(driver);
-        account(driver);
-        languages(driver);
-        search(driver);
-        newListProducts(driver);
-        navigateToPage(driver, "Sale");
-        addProductToCart(driver);
-        removeProductFromCart(driver);
-        submitReview(driver);
-        register(driver);
-        register(driver);
+
+        homepage();
+        account();
+        languages();
+        search();
+        newListProducts();
+        navigateToPage("Sale");
+        addProductToCart();
+        removeProductFromCart();
+        submitReview();
+        register();
+
     }
 }
